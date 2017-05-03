3b73ad7e331ee09bfb7aa98a9686cb1a3a5946b2
pedronoefoker <pedronoe@comunidad.unam.mx>
2017-05-02 23:43:26 -0500

    primera visualización

---
+++ README.md
@@ -0,0 +1,3 @@
+# proyectoWeb2
+Este git contiene un pequeño diseño de como visualizamos la pagina web 
+de proteco utilizando diversos elementos de materialize :3
---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.parallax').parallax();
+});
+
+  $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+ $(document).ready(function(){
+      $('.modal').modal();
+    });
