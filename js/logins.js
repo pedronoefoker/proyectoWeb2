03819d69d2feb6f8143c9f8902ab5483a05a2a89
Yuto-Oro <monkeydoro@gmail.com>
2017-05-07 23:43:02 -0500

    Todos los Archivos minificados

---
+++ js/logins.js
@@ -0,0 +1,50 @@
+(function(d, s, id) 
+{
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+var provedor = new firebase.auth.GoogleAuthProvider();
+provedor.addScope('https://www.googleapis.com/auth/plus.login');
+firebase.auth().signInWithPopup(provider).then(function(result) 
+{
+	// This gives you a Google Access Token. You can use it to access the Google API.
+	var token = result.credential.accessToken;
+	// The signed-in user info.
+	var user = result.user;
+	// ...
+}).catch(function(error) 
+{
+	// Handle Errors here.
+	var errorCode = error.code;
+	var errorMessage = error.message;
+	// The email of the user's account used.
+	var email = error.email;
+	// The firebase.auth.AuthCredential type that was used.
+	var credential = error.credential;
+	// ...
+});
+
+var provedor2 = new firebase.auth.GithubAuthProvider();
+provedor2.addScope('repo');
+firebase.auth().signInWithPopup(provider).then(function(result) 
+{
+  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) 
+{
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
